bdb331d1e37d78683a3c5e0fb459e32b4d65a04b
kminkyung <mminkyung.k@gmail.com>
2019-11-18 13:10:44 +0900

    vue model show for if bind on

---
+++ js/index.js
@@ -0,0 +1,34 @@
+new Vue({
+	el: "#app",
+	data: {
+		title: "vue를 배워봅시다.",
+		str: '',
+		isPrdView: false,
+		items: [
+			{
+				id: 1,
+				title: "연어",
+				desc: "맛있는 연어",
+				src:"../img/salmon.jpg"
+			},
+			{
+				id: 2,
+				title: "체리",
+				desc: "싱싱한 체리",
+				src:"../img/cherries.jpg"
+			},
+			{
+				id: 3,
+				title: "와인",
+				desc: "오래된 와인",
+				src:"../img/wine.jpg"
+			}
+		]
+	},
+	methods: {
+		onTogglePrd(e) {
+			this.isPrdView = !this.isPrdView;
+		}
+	}
+});
+
